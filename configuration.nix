57b4866168981c7b28d0caf4120df39f5d5e7c97
Alban <dev.albanagisa@gmail.com>
2023-09-20 01:03:38 +0200

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,192 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    settings = { General = { ControllerMode = "bredr"; }; };
+  };
+
+  systemd.services.bluetooth.serviceConfig.execStart = [
+	""
+	"${pkgs.bluez}/libexec/bluetooth/bluetoothd --noplugin=sap,avrcp"
+  ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.albanagisa = {
+    isNormalUser = true;
+    description = "albanagisa";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+  # Desktop
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+    mako
+    swww
+    kitty
+    libnotify
+    waybar
+    nodejs_18
+    vscode
+    gnome.seahorse
+    gnome3.gnome-tweaks
+    discord
+    bluez
+    gnome.gnome-boxes
+    google-chrome
+    git
+    onlyoffice-bin
+    virt-manager
+    spice-gtk
+    spice-vdagent
+  ];
+
+  environment.gnome.excludePackages = with pkgs.gnome; [
+        pkgs.gnome-tour
+        pkgs.gnome-photos
+        baobab
+        cheese
+        epiphany
+        gedit
+        simple-scan
+        yelp
+        evince
+        geary
+        gnome-contacts
+        gnome-font-viewer
+        gnome-logs
+        gnome-maps
+        gnome-music
+        gnome-weather
+        pkgs.gnome-connections
+  ];
+
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  services.flatpak.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  security.wrappers.spice-client-glib-usb-acl-helper.source = "${pkgs.spice-gtk}/bin/spice-client-glib-usb-acl-helper";
+  programs.dconf.enable = true;
+
+  environment.sessionVariables = {
+	WLR_NO_HARDWARE_CURSORS = "1";
+	NIXOS_OZONE_WL = "1";
+  };
+
+  programs.hyprland = { 
+	enable = true;
+	nvidiaPatches = true;
+	xwayland.enable = true;
+  };
+
+  hardware = {
+     opengl.enable = true;
+     nvidia.modesetting.enable = true;
+  };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland  pkgs.xdg-desktop-portal-gnome ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+	discord = super.discord.overrideAttrs (
+	_: { src = builtins.fetchTarball {
+	    url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+	    sha256 = "0yzgkzb0200w9qigigmb84q4icnpm2hj4jg400payz7igxk95kqk";
+	  }; }
+	);
+    })
+  ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+	
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
